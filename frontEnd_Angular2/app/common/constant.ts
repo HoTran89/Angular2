7d58b3be11181ed49fb6aa1a3c6ca46481112b8a
Ho Tran <tran.ngoc.ho89@gmail.com>
2016-08-13 21:02:00 +0700

    add constant file

---
+++ frontEnd_Angular2/app/common/constant.ts
@@ -0,0 +1,5 @@
+export const Navigate = {
+    Emails: "Emails",
+    CreateEmail: "Create Email",
+    EditEmail: "Edit Email"
+}
--- frontEnd_Angular2/app/layout.ts
+++ frontEnd_Angular2/app/layout.ts
@@ -2,16 +2,17 @@ import {Component} from "angular2/core";
 import {RouteConfig, RouterOutlet, RouterLink} from "angular2/router";
 import {EmailsComponent} from "./components/emailsComponent";
 import {CreateEmailComponent} from "./components/createEmailComponent";
+import Constant from "./common/constant";
 
 @Component({
     selector: "default-layout",
     templateUrl: "app/layout.html",
-    directives: [RouterOutlet, RouterLink] 
+    directives: [RouterOutlet, RouterLink]
 })
 @RouteConfig([
-    {path: "/emails", name: "Emails", component: EmailsComponent, useAsDefault: true},
-    {path: "/createEmail", name: "Create Email", component: CreateEmailComponent},
-    {path: "/editEmail/:id", name: "Edit Email", component: CreateEmailComponent}
+    { path: "/emails", name: Constant.Navigate.Emails, component: EmailsComponent, useAsDefault: true },
+    { path: "/createEmail", name: Constant.Navigate.CreateEmail, component: CreateEmailComponent },
+    { path: "/editEmail/:id", name: Constant.Navigate.EditEmail, component: CreateEmailComponent }
 ])
 export class DefaultLayout {
 }
